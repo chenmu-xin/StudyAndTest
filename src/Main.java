ac2b68ed2c95d89d815a19bd235eaa052a630e25
chenmu-xin <jellyteam@163.com>
2021-11-15 13:23:49 +0800

    first submit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    // Ctrl + E	显示最近打开的文件记录列表
+    // Ctrl + Alt + L 格式化代码，可以对当前文件和整个包目录使用 （必备）
+    // sout main 自动提示
+
+
+    public static void main(String[] args) {
+        System.out.printf("hello word");
+        System.out.println();
+
+    }
+
+}
