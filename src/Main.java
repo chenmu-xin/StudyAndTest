a18e57751817ee6f96fae097063a556edaa9fd5c
chenmu-xin <jellyteam@163.com>
2021-11-15 13:47:51 +0800

    submit test2

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     // Ctrl + E	显示最近打开的文件记录列表
     // Ctrl + Alt + L 格式化代码，可以对当前文件和整个包目录使用 （必备）
     // sout main 自动提示
-    // Ctrl + Shift + K
-
+    // Ctrl + K     本地commit
+    // Ctrl + Shift + K     github上进行push
 
     public static void main(String[] args) {
         System.out.printf("hello word");
