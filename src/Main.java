40b1568eb36d89e0b28daa78a45d9da5777e7d5a
chenmu-xin <jellyteam@163.com>
2021-11-15 13:42:39 +0800

    submit test

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("hello word");
         System.out.println();
+        System.out.println("second");
 
     }
 
