a316fc828c0f1d61a635c7657dd1100b2e33895b
chenmu-xin <jellyteam@163.com>
2021-11-16 10:41:17 +0800

    java study 2

--- src/JavaBaseStudy/Generics/BaseApply.java
+++ src/JavaBaseStudy/Generics/BaseApply.java
@@ -1,6 +1,6 @@
 package JavaBaseStudy.Generics;
 
-// 泛型类可以有多个 类型变量（心理：什么鬼？）
+// 泛型类可以有多个 类型变量（心理：什么鬼？没懂呀？）
 // 那么...像不像...没错...Map<K,V> !!!! 集合中的 Map 就是泛型类的最好应用
 // 同样，泛型方法就是我们常用的，返回值是 ArrayList<String> 这类的方法啦！
 public class BaseApply<T, U> {
